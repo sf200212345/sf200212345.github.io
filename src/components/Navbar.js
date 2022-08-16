de7d8a6b0533cf17e8e029930bbace0ba9a0da0b
sf200212345 <stevefan@umich.edu>
2022-08-15 20:34:32 -0400

    Resolved #11. Navbar works as desired. May change the navbar look when hidden.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import "./Navbar.css";
 
 const Navbar = () => {
+
+  const notFocused = "nav-item";
+  const Focused = "nav-item focused";
+  const introduction = document.getElementById("introduction");
+  const resume = document.getElementById("resume");
+  const projects = document.getElementById("projects");
+
+  const [scrollPosition, setScrollPosition] = useState(window.pageYOffset);
+  const [isScrollDown, setIsScrollDown] = useState(false);
+  
+  const handleScrollCallback = useCallback(
+    () => {
+      setIsScrollDown(window.pageYOffset > scrollPosition);
+      setScrollPosition(window.pageYOffset);
+    }, [scrollPosition]
+  );
+
+  useEffect(
+    () => {
+      window.addEventListener("scroll", handleScrollCallback, {passive: true});
+      return () => window.removeEventListener("scroll", handleScrollCallback);
+    }, [scrollPosition, handleScrollCallback]
+  );
+
   return (
-    <header className='navbar'>
-      <div className='nav-item'>
-        <a href='#introduction' className='nav-link'>Introduction</a>
-      </div>
-      <div className='nav-item'>
-        <a href="#resume" className='nav-link'>Resume</a>
-      </div>
-      <div className='nav-item'>
-        <a href='#projects' className='nav-link'>Projects</a>
-      </div>
-    </header>
+    <div className={isScrollDown ? " hidden navbar-container" : "navbar-container"}>
+      <header className="navbar">
+        <div className={scrollPosition <= window.innerHeight * 0.60 ? Focused : notFocused} onClick={() => introduction.scrollIntoView()}>Introduction</div>
+        <div className={scrollPosition > window.innerHeight * 0.60 && scrollPosition <= window.innerHeight * 1.60 ? Focused : notFocused} onClick={() => resume.scrollIntoView()}>Resume</div>
+        <div className={scrollPosition > window.innerHeight * 1.60 ? Focused : notFocused} onClick={() => projects.scrollIntoView()}>Projects</div>
+      </header>
+    </div>
   );
 };
 
