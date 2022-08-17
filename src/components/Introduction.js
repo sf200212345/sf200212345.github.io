6fa48a1a230d8998c0cd271c981894f34ac85e7b
sf200212345 <stevefan@umich.edu>
2022-08-17 17:57:46 -0400

    Resolved #14. Could add further stylistic elements to the introduction component.

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -3,7 +3,12 @@ import React from 'react';
 const Introduction = () => {
   return (
     <div className='section' id='introduction'>
-      Introduction
+      <div className='pp'>
+        <img src={require('../images/headshot.jpg')} alt='headshot.jpg' />
+      </div>
+      <div className='bio'>
+        Adaptable rising Junior working on a B.S. in Computer Science and seeking to engage with meaningful projects in a Summer 2022 internship. Inquisitive, hard-working, and familiar with working in project teams. Comfortable working both virtually and in-person.
+      </div>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import "./Navbar.css";
+import "./styles/Navbar.css";
 
 const Navbar = () => {
 
