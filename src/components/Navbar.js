31d38d8d9394dcbda49a400f2f3b78ae0fef1951
sf200212345 <stevefan@umich.edu>
2022-08-17 17:12:43 -0400

    Fixed bug that prevented the navbar buttons working on page load.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,6 @@ const Navbar = () => {
 
   const notFocused = "nav-item";
   const Focused = "nav-item focused";
-  const introduction = document.getElementById("introduction");
-  const resume = document.getElementById("resume");
-  const projects = document.getElementById("projects");
 
   const [scrollPosition, setScrollPosition] = useState(window.pageYOffset);
   const [isScrollDown, setIsScrollDown] = useState(false);
@@ -27,11 +24,11 @@ const Navbar = () => {
   );
 
   return (
-    <div className={isScrollDown ? " hidden navbar-container" : "navbar-container"}>
+    <div className={isScrollDown ? "hidden navbar-container" : "navbar-container"}>
       <header className="navbar">
-        <div className={scrollPosition <= window.innerHeight * 0.60 ? Focused : notFocused} onClick={() => introduction.scrollIntoView()}>Introduction</div>
-        <div className={scrollPosition > window.innerHeight * 0.60 && scrollPosition <= window.innerHeight * 1.60 ? Focused : notFocused} onClick={() => resume.scrollIntoView()}>Resume</div>
-        <div className={scrollPosition > window.innerHeight * 1.60 ? Focused : notFocused} onClick={() => projects.scrollIntoView()}>Projects</div>
+        <div className={scrollPosition <= window.innerHeight * 0.60 ? Focused : notFocused} onClick={() => document.getElementById("introduction").scrollIntoView()}>Introduction</div>
+        <div className={scrollPosition > window.innerHeight * 0.60 && scrollPosition <= window.innerHeight * 1.60 ? Focused : notFocused} onClick={() => document.getElementById("resume").scrollIntoView()}>Resume</div>
+        <div className={scrollPosition > window.innerHeight * 1.60 ? Focused : notFocused} onClick={() => document.getElementById("projects").scrollIntoView()}>Projects</div>
       </header>
     </div>
   );
