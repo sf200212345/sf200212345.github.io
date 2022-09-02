84567760f19f84a0bd5b7ea35b62558c52e1a1c4
sf200212345 <stevefan@umich.edu>
2022-09-01 23:24:03 -0400

    Created structure for project popups, still need to add content and styles

--- src/components/ProjectsPopup.js
+++ src/components/ProjectsPopup.js
@@ -13,8 +13,17 @@ const ProjectsPopup = ({ info }) => {
             (<div className='popup-wrapper'>
                 <OutsideClickHandler onOutsideClick={() => setClicked(false)}>
                     <div className='popup'>
-                        {info.mainImage}
-                        {info.title}-{info.for}{info.date}
+                        <button className='exit-btn' onClick={() => setClicked(false)}><i className="fa-solid fa-x"></i></button>
+                        <div className='img-container'><img src={require('../images/headshot.jpg')} alt='headshot.jpg' /></div>
+                        <div className='text-container'>
+                            <h3>{info.title}</h3><span className='date'>{info.date}</span><br />
+                            <span className='italic'>{info.for}</span>
+                            <p className='desc'>{info.desc}</p>
+                            <span className='builtWith'>{info.builtWith}</span>
+                            <div className='icon-links'>
+                                {info.links.map(curr => (<a key={curr.link} href={curr.link} target="_blank" rel="noopener noreferrer"><i className={curr.class}></i></a>))}
+                            </div>
+                        </div>
                     </div>
                 </OutsideClickHandler>
             </div>)
